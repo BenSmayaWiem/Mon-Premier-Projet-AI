973809a5eeb15a99b6fdfd49b4000f922c603851
BenSmayaWiem <51333032+BenSmayaWiem@users.noreply.github.com>
2019-12-29 19:13:17 +0100

    Update model.py

--- model.py
+++ model.py
@@ -13,7 +13,7 @@ X = dataset.iloc[:, :3]
 #Converting words to integer values
 def convert_to_int(word):
     word_dict = {'zero':0, 'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8,
-                'nine':9, 'ten':10, 'eleven':11, 'twelve':12}
+                'nine':9, 'ten':10, 'eleven':11, 'twelve':12,'thirteen':13, 'fourteen':14}
     return word_dict[word]
 
 X['experience'] = X['experience'].apply(lambda x : convert_to_int(x))
